b4a43167343358e9fbf6fc74d42db7b060916117
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2022-09-03 14:08:37 +0000

    [Add]usersテーブルにカラム追加途中

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2022_08_31_133810) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
