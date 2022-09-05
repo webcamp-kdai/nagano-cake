3117661581c7c14e7548f6257517cc33513e0125
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2022-09-05 02:32:37 +0000

    [Add]usersカラム

--- db/migrate/20220903140519_add_address_to_users.rb
+++ db/migrate/20220903140519_add_address_to_users.rb
@@ -1,5 +1,11 @@
 class AddAddressToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :portal_code, :string
+    add_column :users, :address, :string
+    add_column :users, :telephone_number, :string
+
+    add_column :users, :is_deleted, :boolean , null: false, default: false
+    add_column :users, :created_at, :datetime , null: false, default: -> {"now()"}
+    add_column :users, :updated_at, :datetime , null: false, default: -> {"now()"}
   end
 end
