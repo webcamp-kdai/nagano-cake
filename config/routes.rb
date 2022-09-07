b383f7e98ca0d4db80afb0a58586ad32d6fc317f
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2022-09-07 14:12:13 +0000

    [add]genre model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   devise_for :admin,skip:[:registrations,:passwords],controllers:{
     sessions:"admin/sessions"
   }
--- test/models/genre_test.rb
+++ test/models/genre_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Public::GenresControllerTest < ActionDispatch::IntegrationTest
+class GenreTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
