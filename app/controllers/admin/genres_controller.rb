f928dbdbb121182a2ce432e36a52128397e040b1
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2022-09-08 13:13:31 +0000

    [Add]genres index view

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,26 @@
 class Admin::GenresController < ApplicationController
+
+
+  def index
+    @genres = Genres.all
+  end
+
+  def edit
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to genre_path
+  end
+
+  def update
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ Rails.application.routes.draw do
     registrations:"public/registrations",
     sessions:"public/sessions"
   }
+  
+  resources:genres,only:[:create,:index,:edit,:update]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
