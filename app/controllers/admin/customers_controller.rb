f355e427963de2619c675f8b9bcfe09431cba4e7
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2022-09-12 15:37:27 +0000

    [Add]顧客詳細、編集ページ作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -11,6 +11,12 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
 
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update
+    redirect_to admin_customer_path
+  end
+
   private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
