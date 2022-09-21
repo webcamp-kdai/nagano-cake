cf1981dec58ff829eecc2c0870a929d13760e436
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2022-09-21 11:50:39 +0000

    [add]admin genre,item model validates等記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached:image
+  belongs_to :genre
+
+  validates:name,presence:true
+  validates:introduction,presence:true
+  validates:price,presence:true
 
   def get_image
     unless image.attached?
