1c89c5ff5dea8f11e05a67992e44077a547da2b1
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2022-09-22 14:49:37 +0000

    [add]top about

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/unsubscribe'
+  end
     root 'public/homes#top'
     get "/about" => "public/homes#about",as:"about"
 
